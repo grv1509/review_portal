bcfb6f1166aab223e2ab6b90312e7dcc6617f14f
Gaurav Verma <gaurav.verma1509@gmail.com>
2016-10-27 02:16:41 +0530

    some layout changes

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -7,16 +7,17 @@ class ReviewsController < ApplicationController
 
 	def new
 		@hotel = Hotel.find_by_id(params[:hotel_id])
-		@review = @hotel.reviews.new
+		@review = Review.new
 	end
 
 	def create
 		@hotel = Hotel.find_by_id(params[:hotel_id])
-		review = @hotel.reviews.new(review_params)
+		review = Review.create(review_params)
 		# picking first hardcoded user for now
 		# review.user_id = current_user.id
 		review.user_id = User.first.id
-		if @hotel.reviews.new(review_params)
+		review.hotel_id = @hotel.id
+		if review.save
       flash[:success] = "Review saved !!"
       redirect_to hotels_path
     else
@@ -27,6 +28,6 @@ class ReviewsController < ApplicationController
 	private
 
 	def review_params
-    params.require(:review).permit(:stars, :label, :description)
-  end
+		params.require(:review).permit(:stars, :label, :description)
+	end
 end
